027293fe991742397a02654c4d5586f1e1b74d58
giobocca03 <gio.bocca03@gmail.com>
2025-01-23 10:43:31 +0100

    cambiato leggermente html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
-function convertDate(date){
+const newsContainer = document.querySelector("[data-news-container]")
+
+function convertiDate(date){
   let data = new Date(date * 1000);
       
   const dataItaliana = data.toLocaleString('it-IT', {
@@ -13,16 +15,26 @@ function convertDate(date){
 
   return dataItaliana
 }
+
+function creaNews(news){
+
+}
+
+async function getHakersNwsData(response) {
+  for (let i = 0 ; i<10;i++){
+    const result = await axios.get("https://hacker-news.firebaseio.com/v0/item/"+response.data[i]+".json ")
+    console.log(result)
+    console.log(result.data.title+ " " + result.data.url)
+    console.log(convertiDate(result.data.time))
+  }
+}
+
+
 async function getHakerNewsId(){
   try{
     const response = await axios.get("https://hacker-news.firebaseio.com/v0/newstories.json")
     console.log(response.data[0]);
-    for (let i = 0 ; i<10;i++){
-      const result = await axios.get("https://hacker-news.firebaseio.com/v0/item/"+response.data[i]+".json ")
-      console.log(result)
-      console.log(result.data.title+ " " + result.data.url)
-      console.log(convertDate(result.data.time))
-    }
+    getHakersNwsData(response)
   } catch (error) {
     console.log(error)
   }
